e1509d4405b9d6d7b806f369c7b0bbdbbb7da2c9
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-12-04 16:01:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,10 @@ class Student
   
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, album) 
+      INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   
 end
